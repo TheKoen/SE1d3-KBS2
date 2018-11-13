68ca39ccc9c78158e29bc80911106ea5cefd69d7
Tom Verra <tom.verra@hotmail.com>
2018-11-13 16:15:41 +0100

    Customer Road functions

--- KBS2/Customer/CustomerGroup.cs
+++ KBS2/Customer/CustomerGroup.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace KBS2.Customer
 {
@@ -12,6 +13,7 @@ namespace KBS2.Customer
         public List<Customer> Customers { get; set; } = new List<Customer>();
         public Building Destination { get; set; }
         public CustomerGroupController Controller { get; set; }
+        public Vector Location { get; set; } 
 
         public CustomerGroup(int customers, Building start, Building destination)
         {
@@ -19,7 +21,7 @@ namespace KBS2.Customer
             {
                 Customers.Add(new Customer(start.location, random.Next(4, 90), start));
             }
-
+            Location = start.location;
             Destination = destination;
             Controller = new CustomerGroupController(this);
         }
