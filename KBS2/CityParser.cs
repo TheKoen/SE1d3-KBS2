3f93f6f241a19b856b2c62ff726ceec77109ac41
s1118049 <38284664+s1118049@users.noreply.github.com>
2018-11-13 15:44:11 +0100

    City Parses

---
+++ KBS2/CityParser.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Xml;
+
+namespace KBS2
+{
+    class CityParser
+    {
+        public static City MakeCity(XmlDocument city)
+        {
+            var cityObject = new City();
+
+            var root = city.DocumentElement;
+            if (root == null) throw new XmlException("Missing root node");
+
+            //selecting and adding roads to List
+            var roads = city.SelectSingleNode("//City/Roads");
+            if (roads == null)
+                throw new XmlException("Missing roads in city.");
+            foreach (var road in roads.ChildNodes)
+            {
+                cityObject.Roads.Add(ParseRoad((XmlNode)road));
+            }
+
+            //selecting and adding buildings to List
+            var buildings = city.SelectSingleNode("//City/Buildings");
+            if (buildings == null)
+                throw new XmlException("Missing buildings in city.");
+            foreach (var building in buildings.ChildNodes)
+            {
+                cityObject.Buildings.Add(ParseBuilding((XmlNode)building));
+            }
+
+            return cityObject;
+        }
+
+        public static Vector ParseLocation(string locationString)
+        {
+            var split = locationString.Split(',');
+            return new Vector(int.Parse(split[0]), int.Parse(split[1]));
+        }
+
+        public static Road ParseRoad(XmlNode node)
+        {
+            var start = ParseLocation(node.Attributes["Start"].InnerText);
+            var end = ParseLocation(node.Attributes["End"].InnerText);
+            var width = int.Parse(node.Attributes["Width"].InnerText);
+            var maxspeed = int.Parse(node.Attributes["MaxSpeed"].InnerText);
+
+            return new Road(start, end, width, maxspeed);
+        }
+
+        public static Building ParseBuilding(XmlNode node)
+        {
+            var loc = ParseLocation(node.Attributes["Location"].InnerText);
+            var size = int.Parse(node.Attributes["Size"].InnerText);
+
+            return new Building(loc, size);
+        }
+
+    }
+}
