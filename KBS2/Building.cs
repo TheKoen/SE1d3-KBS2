768bba6a5e5c74b5acec2728f0ef8d05d3e6bf5b
s1118049 <38284664+s1118049@users.noreply.github.com>
2018-11-13 14:51:13 +0100

    City package met properties en constructor

--- KBS2/Building.cs
+++ KBS2/Building.cs
@@ -13,5 +13,10 @@ namespace KBS2
         public Vector Location { get; set; }
         public int Size { get; set; }
 
+        public Building(Vector l, int s)
+        {
+            this.Location = l;
+            this.Size = s;
+        }
     }
 }
--- KBS2/Road.cs
+++ KBS2/Road.cs
@@ -13,5 +13,13 @@ namespace KBS2
         public Vector End { get; set; }
         public int Width { get; set; }
         public int MaxSpeed { get; set; }
+
+        public Road(Vector s, Vector e, int w, int ms)
+        {
+            this.Start = s;
+            this.End = e;
+            this.Width = w;
+            this.MaxSpeed = ms;
+        }
     }
 }
