8b74a387a64f241ec2f9a6f2929e00e73e2f9190
s1118049 <38284664+s1118049@users.noreply.github.com>
2018-11-13 13:58:36 +0100

    City package

---
+++ KBS2/City.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KBS2
+{
+    class City
+    {
+        public List<Road> roads = new List<Road>();
+        public List<Building> buildings = new List<Building>();
+        public List<Car> cars = new List<Car>();
+
+    }
+}
---
+++ KBS2/Road.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace KBS2
+{
+    class Road
+    {
+        public Vector start;
+        public Vector end;
+        public int width;
+        public int maxSpeed;
+    }
+}
