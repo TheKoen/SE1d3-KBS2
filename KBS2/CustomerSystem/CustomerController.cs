ce62bdc0e93dd92f15dfd0e62e04c163d1738dba
Tom Verra <tom.verra@hotmail.com>
2018-11-14 13:03:40 +0100

    Customers move to group

--- KBS2/CustomerSystem/CustomerController.cs
+++ KBS2/CustomerSystem/CustomerController.cs
@@ -1,4 +1,8 @@
-﻿namespace KBS2.CustomerSystem
+﻿using KBS2.Util;
+using System.Windows;
+
+namespace KBS2.CustomerSystem
+
 {
     public class CustomerController
     {
@@ -6,10 +10,24 @@
 
         public CustomerController(Customer customer)
         {
-            Customer = customer;
-
+            Customer = customer;            
         }
 
-        public void Update() { }
+        public double WhereIsMyGroup()
+        {
+            return VectorUtil.Distance(Customer.Group.Location, Customer.Location);
+        }
+
+        public void Update() {
+            var gL = Customer.Group.Location;
+            var cL = Customer.Location;
+
+            if (WhereIsMyGroup() > 40)
+            {
+                Vector delta = new Vector(gL.X - cL.X, gL.Y - cL.X);
+                delta.Normalize();
+                Customer.Location = Vector.Add(Customer.Location, delta);
+            };
+        }
     }
 }
