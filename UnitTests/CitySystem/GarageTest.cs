c9637f1302ad59dff9f655d1566a1e7e02b0c796
s1118049 <38284664+s1118049@users.noreply.github.com>
2018-11-19 13:28:13 +0100

    unittest garage

---
+++ UnitTests/CitySystem/GarageTest.cs
@@ -0,0 +1,40 @@
+﻿using KBS2.CarSystem;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using UnitTests.Util;
+
+namespace UnitTests.CitySystem
+{
+    [TestFixture]
+    public class GarageTest
+    {
+
+        [TestCase(40, 50, DirectionCar.South)]
+        [TestCase(40, 50, DirectionCar.North)]
+        [TestCase(60, 60, DirectionCar.East)]
+        [TestCase(60, 60, DirectionCar.West)]
+
+
+
+
+        public void TestSpawnCar(double gx, double gy, DirectionCar direction)
+        {
+            
+            var city = new CityBuilder()
+                .Road(new Vector(10, 0), new Vector(10, 100), 10)
+                .Road(new Vector(0, 90), new Vector(100, 90), 10)
+                .Build();
+
+            var garage = new Garage(new Vector(gx, gy), 20, direction);
+            city.Buildings.Add(garage);
+
+            garage.SpawnCar(0, CarModel.TestModel);
+        }
+
+    }
+}
