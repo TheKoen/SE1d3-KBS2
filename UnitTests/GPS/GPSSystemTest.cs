0c76e051a06abed9c9504b7ad0fb96430397e3b4
GhostMonkeyFPS <33627154+GhostMonkeyFPS@users.noreply.github.com>
2018-11-21 14:35:00 +0100

    Unit test GPS

--- UnitTests/GPS/GPSSystemTest.cs
+++ UnitTests/GPS/GPSSystemTest.cs
@@ -36,9 +36,26 @@ namespace UnitTests.GPS
         }
         
         [TestCase()]
-        public void NearestRoadTest()
+        public void NearestRoadTest(int x1b, int y1b, int x1e, int y1e, int x2b, int y2b, int x2e, int y2e, int ex, int ey)
         {
-            var road1 = new Road()
+            var road1 = new Road(new Vector(x1b, y1b), new Vector(x1e, y1e), 20, 100);
+            var road2 = new Road(new Vector(x2b, y2b), new Vector(x2e, y2e), 20, 100);
+
+            var city = new CityBuilder()
+                .Road(road1)
+                .Road(road2)
+                .Build();
+
+            var road = GPSSystem.NearestRoad(new Vector(ex, ey));
+
+            if(road.Equals(road1))
+            {
+                Assert.Pass();
+            }
+            else
+            {
+                Assert.Fail("estamited is not equal to the first road");
+            }
         }
     }
 }
