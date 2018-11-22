1a3b69206d52ab6661c5f149062466df913566d4
GhostMonkeyFPS <33627154+GhostMonkeyFPS@users.noreply.github.com>
2018-11-22 14:11:50 +0100

    Sensor remove stringConverters

--- KBS2/MainWindow.xaml.cs
+++ KBS2/MainWindow.xaml.cs
@@ -68,8 +68,8 @@ namespace KBS2
             CommandHandler.RegisterCommand("Export", new CommandExport());
             CommandHandler.RegisterCommand("Set", new CommandSet());
             CommandHandler.RegisterCommand("Map", new CommandMap());
-            CommandHandler.RegisterCommand("Sensor", new CommandSetSensor());
-
+            CommandHandler.RegisterCommand("Sensor", new CommandsSensors());
+            
             // Console logic
             MainConsole.SendCommand += (sender, args) =>
             {
