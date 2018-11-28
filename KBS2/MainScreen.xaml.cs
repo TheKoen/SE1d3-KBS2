5766249ce490504104e0379435038656cc7228d0
s1118049 <38284664+s1118049@users.noreply.github.com>
2018-11-28 14:15:36 +0100

    iks em el xml

---
+++ KBS2/MainScreen.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace KBS2
+{
+    /// <summary>
+    /// Interaction logic for MainScreen.xaml
+    /// </summary>
+    public partial class MainScreen : Window
+    {
+        public MainScreen()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnSelect_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnLoad_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnStart_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnPause_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnStop_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
