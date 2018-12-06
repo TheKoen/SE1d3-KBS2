2aa348fd91a3f7644e46e8600ff5025266dbae3b
Koen <koenwillemse1@gmail.com>
2018-12-06 11:42:16 +0100

    Started refactor

--- KBS2/MainScreen.xaml.cs
+++ KBS2/MainScreen.xaml.cs
@@ -46,9 +46,10 @@ namespace KBS2
 
         private readonly ConsoleWindow consoleWindow;
 
-        private readonly CityRenderHandler cityRenderHandler;
-
-        private string filePath;
+        public CityRenderHandler CityRenderHandler { get; private set; }
+        public CustomerRenderHandler CustomerRenderHandler { get; private set; }
+        public SimulationControlHandler SimulationControlHandler { get; private set; }
+        public PropertyDisplayHandler PropertyDisplayHandler { get; private set; }
 
         public int Ticks { get; set; }
         public double SecondsRunning { get; private set; }
@@ -56,16 +57,22 @@ namespace KBS2
         public MainScreen()
         {
             consoleWindow = new ConsoleWindow();
-
-            cityRenderHandler = new CityRenderHandler(CanvasMain);
-
-            InitializeComponent();
-            WPFLoop.Subscribe(Update);
             CommandLoop.Start();
+            
+            Initialized += (sender, args) => Initialize();
+            InitializeComponent();
+        }
+
+        private void Initialize()
+        {
             GPSSystem.Setup();
 
-            // Showing list of properties in settings tab
-            Loaded += (sender, args) => UpdatePropertyList();
+            CityRenderHandler = new CityRenderHandler(this, CanvasMain);
+            CustomerRenderHandler = new CustomerRenderHandler(CanvasMain);
+            SimulationControlHandler = new SimulationControlHandler(this);
+            PropertyDisplayHandler = new PropertyDisplayHandler(this);
+
+            WPFLoop.Subscribe(Update);
         }
 
         protected override void OnClosing(CancelEventArgs e)
@@ -76,101 +83,27 @@ namespace KBS2
 
         private void BtnSelect_Click(object sender, RoutedEventArgs e)
         {
-            var dlg = new Microsoft.Win32.OpenFileDialog
-            {
-                DefaultExt = ".xml",
-                Filter = "XML documents (.xml)|*.xml"
-            };
-
-            // Display OpenFileDialog by calling ShowDialog method
-            var result = dlg.ShowDialog();
-
-            // Get the selected file name and display in a TextBox
-            if (result == true)
-            {
-                
-                // Open document
-                var fileName = dlg.FileName;
-                filePath = fileName;
-                var cityname = Path.GetFileNameWithoutExtension(fileName);
-                TBCity.Text = cityname;
-                CityName.Content = "City : " + cityname.First().ToString().ToUpper() + cityname.Substring(1);
-            }
+            SimulationControlHandler.SelectButtonClick();
         }
 
         private void BtnLoad_Click(object sender, RoutedEventArgs e)
         {
-            // Loads the city file and parses the information into a City.
-            var file = new XmlDocument();
-            file.Load(filePath);
-            CityParser.MakeCity(file);
-            var city = City.Instance;
-
-            //TODO: Draw city.
-
-            UpdatePropertyList();
-            //Enables buttons and tabs so the user can acces them.
-            BtnStart.IsEnabled = true;
-            BtnPause.IsEnabled = true;
-            BtnStop.IsEnabled = true;
-            TabItemSettings.IsEnabled = true;
-            TabItemResults.IsEnabled = true;
-
-            // Fills in the current City information that is needed.
-            LabelSimulationRoad.Content = city.Roads.Count;
-            LabelSimulationIntersection.Content = city.Intersections.Count;
-            LabelSimulationBuilding.Content = city.Buildings.Count;
-            LabelSimulationGarage.Content = city.Buildings.FindAll(building => building is Garage).Count;
-            LabelSimulationAmountCostumer.Content = city.Customers.Count;
+            SimulationControlHandler.LoadButtonClick();
         }
 
         private void BtnStart_Click(object sender, RoutedEventArgs e)
         {
-            WPFLoop.Start();
-            AILoop.Start();
-            App.Console.Print("Start pressed");
-
-            var city = City.Instance;
-            foreach (var Customer in city.Customers)
-            {
-                CanvasMain.Children.Add(Customer.CustomerControl);
-            }
-
-            foreach (var car in city.Cars)
-            {
-                var carcontrol = new CarControl(car);
-                CanvasMain.Children.Add(carcontrol);
-            }
+            SimulationControlHandler.StartButtonClick();
         }
 
         private void BtnPause_Click(object sender, RoutedEventArgs e)
         {
-            WPFLoop.Stop();
-            AILoop.Stop();
-            App.Console.Print("Pause pressed");
+            SimulationControlHandler.PauseButtonClick();
         }
 
         private void BtnStop_Click(object sender, RoutedEventArgs e)
         {
-            WPFLoop.Stop();
-            AILoop.Stop();
-            City.Instance.Controller.Reset();
-            App.Console.Print("Reset pressed");
-        }
-
-        // Creates a label for every property.
-        public void UpdatePropertyList()
-        { 
-            StackPanelSettings.Children.Clear();
-            var properties = CommandHandler.GetProperties();
-            foreach (var property in properties)
-            {
-                var propname = property.Key.ToString();
-                var propvalue = property.Value.Value.ToString();
-
-                var prop = new PropertySettings(propname, propvalue);
-                StackPanelSettings.Children.Add(prop);
-            }
+            SimulationControlHandler.ResetButtonClick();
         }
 
         private void BtnImport_Click(object sender, RoutedEventArgs e)
@@ -236,16 +169,7 @@ namespace KBS2
         private void BtnDefault_Click(object sender, RoutedEventArgs e)
         {
             //TODO: Does this need to be hardcoded? Can't we use a more dynamic solution?
-            CommandHandler.ModifyProperty("main.tickRate", 30);
-            CommandHandler.ModifyProperty("command.tickRate", 30);
-            CommandHandler.ModifyProperty("startingPrice", 1.50);
-            CommandHandler.ModifyProperty("pricePerKilometer", 1.00);
-            CommandHandler.ModifyProperty("customerSpawnRate", 0.2f);
-            CommandHandler.ModifyProperty("availableCars", 10);
-            CommandHandler.ModifyProperty("customerCount", 10);
-            CommandHandler.ModifyProperty("globalSpeedLimit", -1);
-            CommandHandler.ModifyProperty("avgGroupSize", 10);
-            UpdatePropertyList();
+            
         }
 
         public void UpdateTimer()
---
+++ KBS2/Visual/PropertyDisplayHandler.cs
@@ -0,0 +1,59 @@
+﻿using System.Windows.Controls;
+using KBS2.Console;
+
+namespace KBS2.Visual
+{
+    public class PropertyDisplayHandler
+    {
+        private StackPanel PropertyPanel { get; }
+
+        private int ticks;
+
+        public PropertyDisplayHandler(MainScreen screen)
+        {
+            PropertyPanel = screen.StackPanelSettings;
+
+            MainScreen.WPFLoop.Subscribe(Update);
+            screen.Loaded += (sender, args) => UpdateProperties();
+        }
+
+        public void UpdateProperties()
+        {
+            PropertyPanel.Children.Clear();
+
+            var properties = CommandHandler.GetProperties();
+            foreach (var property in properties)
+            {
+                var propertyName = property.Key;
+                var propertyValue = property.Value.Value.ToString();
+
+                PropertyPanel.Children.Add(new PropertySettings(propertyName, propertyValue));
+            }
+        }
+
+        public void ResetDefaults()
+        {
+            CommandHandler.ModifyProperty("main.tickRate", 30);
+            CommandHandler.ModifyProperty("command.tickRate", 30);
+            CommandHandler.ModifyProperty("startingPrice", 1.50);
+            CommandHandler.ModifyProperty("pricePerKilometer", 1.00);
+            CommandHandler.ModifyProperty("customerSpawnRate", 0.2f);
+            CommandHandler.ModifyProperty("availableCars", 10);
+            CommandHandler.ModifyProperty("customerCount", 10);
+            CommandHandler.ModifyProperty("globalSpeedLimit", -1);
+            CommandHandler.ModifyProperty("avgGroupSize", 10);
+
+            UpdateProperties();
+        }
+
+        private void Update()
+        {
+            ticks++;
+            if (ticks == 50)
+            {
+                ticks = 0;
+                UpdateProperties();
+            }
+        }
+    }
+}
