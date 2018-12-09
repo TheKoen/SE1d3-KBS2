988a1b1247cf0e7c95645fda511e80ab89013472
Dennis Kok <dennismonnetje@gmail.com>
2018-12-09 19:06:17 +0100

    Nothing escapes the Linqification

--- CommandSystem/CommandRegistrar.cs
+++ CommandSystem/CommandRegistrar.cs
@@ -9,21 +9,15 @@ namespace CommandSystem
         /// Looks for classes with the <see cref="CommandMetadataAttribute"/> attribute and registers them if they are set to AutoRegister
         /// </summary>
         /// <param name="namespace">The namespace to scan for classes</param>
-        public static void AutoRegisterCommands(string @namespace)
-        {
+        public static void AutoRegisterCommands(string @namespace) =>
             // Getting all classes in this domain that implement ICommand and are in the given namespace
-            var types =  AppDomain.CurrentDomain.GetAssemblies()
+            AppDomain.CurrentDomain.GetAssemblies()
                 .SelectMany(t => t.GetTypes())
                 .Where(t => t.IsClass && t.Namespace == @namespace && typeof(ICommand).IsAssignableFrom(t))
-                .Where(t => Attribute.IsDefined(t, typeof(CommandMetadataAttribute)));
-
-            foreach (var type in types)
-            {
-                // Registering the command if it has AutoRegister set
-                var attr = (CommandMetadataAttribute)Attribute.GetCustomAttribute(type, typeof(CommandMetadataAttribute));
-                if (attr.AutoRegister)
-                    CommandHandler.RegisterCommand(type);
-            }
-        }
+                .Where(t => Attribute.IsDefined(t, typeof(CommandMetadataAttribute)))
+                .Where(t =>
+                    ((CommandMetadataAttribute) Attribute.GetCustomAttribute(t, typeof(CommandMetadataAttribute)))
+                    .AutoRegister)
+                .ToList().ForEach(CommandHandler.RegisterCommand);
     }
 }
